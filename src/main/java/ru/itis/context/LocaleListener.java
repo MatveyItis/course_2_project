da54470d638ab6d24c9716d60b140629adf1b104
matveymaletskov <maletskovitis@mail.ru>
2018-10-15 20:59:59 +0300

    add localization package

---
+++ src/main/java/ru/itis/context/LocaleListener.java
@@ -0,0 +1,11 @@
+package ru.itis.context;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class LocaleListener implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+
+    }
+}
